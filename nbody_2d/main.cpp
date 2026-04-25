5d13ebc087d86162d00915e68b10913a9bbf1d7b
Nicolas Garcia <nicolas.garcia.r@usach.cl>
2026-04-24 23:37:22 -0400

    Serial computeAccelerations, random system, and calculate energy, optimized

--- nbody_2d/main.cpp
+++ nbody_2d/main.cpp
@@ -6,12 +6,12 @@ int main () {
     //si ya existe makefile borrar este comentario y el de abajo.
     //para compilar g++ Particle.h Particle.cpp NBodySystem.h NBodySystem.cpp NBodySimulator.h NBodySimulator.cpp main.cpp -o test.exe
 
-    int seed = 31;
-    int nParticles = 100;
+    int seed = 32;
+    int nParticles = 500;
     double dt = 0.01; //que tan grande va a ser el movimiento de las particulas en cada paso
     double G = 1.0;
     double epsilon = 0.05;
-    int steps = 1000;
+    int steps = 200;
 
     NBodySystem system(G, epsilon);
 
